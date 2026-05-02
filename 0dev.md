9c6ceca90a4c38d0a5242d26b7ff6f9221217cd2
Sven Ehrke <sven.ehrke@sven-ehrke.de>
2026-05-02 06:55:07 +0200

    some examples and settings for code documentation

---
+++ 0dev.md
@@ -0,0 +1,10 @@
+# DevDocs
+
+## How this folder was created
+
+From https://starlight.astro.build/getting-started/
+
+`npm create astro@latest -- --template starlight`
+
+`npm run dev`
+
--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,9 @@ export default defineConfig({
 	integrations: [
 		starlight({
 			title: 'My Docs',
+			customCss: [
+        		'./src/styles/custom.css',
+      		],
 			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
 			sidebar: [
 				{
