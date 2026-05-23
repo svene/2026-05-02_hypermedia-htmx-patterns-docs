49b0c7e8b2a8dcb8c796914f35a853a4b6e78683
Sven Ehrke <sven.ehrke@sven-ehrke.de>
2026-05-23 14:28:07 +0200

    continued with Hono documentation

--- extract-snippets/extract-hono-snippets.js
+++ extract-snippets/extract-hono-snippets.js
@@ -12,7 +12,12 @@ export function extractHonoSnippets() {
   // Components:
   processSnippet(
     `${components}/helloworld.ts`,
-    { allowedTags: ['component'], transformFn: snippetToCodeBlock(`html title="helloworld.ts"`), outFile: `${outRoot}/components/helloworld_ts.mdx` }
+    { allowedTags: ['component'], transformFn: snippetToCodeBlock(`html title="helloworld.ts"`), outFile: `${outRoot}/components/hono/helloworld_ts.mdx` }
+  );
+
+  processSnippet(
+    `${components}/helloworldparams.ts`,
+    { allowedTags: ['component'], transformFn: snippetToCodeBlock(`html title="helloworldparams.ts"`), outFile: `${outRoot}/components/hono/helloworldparams_ts.mdx` }
   );
 
   // M01D01:
@@ -25,6 +30,12 @@ export function extractHonoSnippets() {
   processSnippet(
     `${m01}/m01d02.ts`,
     { allowedTags: ['page'], transformFn: snippetToCodeBlock(`html title="m01d02.ts"`), outFile: `${outRoot}/hono/m01d02_ts.mdx` }
+  );
+
+  // M01D03:
+  processSnippet(
+    `${m01}/m01d03.ts`,
+    { allowedTags: ['page'], transformFn: snippetToCodeBlock(`html title="m01d03.ts"`), outFile: `${outRoot}/hono/m01d03_ts.mdx` }
   );
 
 
