270ce9138a983563ee92c94144e50dc6ff697b2a
Sven Ehrke <sven.ehrke@sven-ehrke.de>
2026-05-24 05:27:24 +0200

    structure for Hono/JSX

--- extract-snippets/extract-hono-snippets.js
+++ extract-snippets/extract-hono-snippets.js
@@ -5,8 +5,6 @@ export function extractHonoSnippets() {
   const srcRoot = '../../2025/2025-12-27_ssfe-patterns-hono-htmx';
   const outRoot = 'generated/snippets';
 
-  // M01:
-  let m01 = `${srcRoot}/src/m01html`;
   let components = `${srcRoot}/src/components`;
 
   // Components:
@@ -20,8 +18,9 @@ export function extractHonoSnippets() {
   component('helloworldparams');
   component('helloworldcontent');
 
-  // M01D01:
+  // M01:
   const m01page = n => {
+    const m01 = `${srcRoot}/src/m01html`;
     processSnippet(
       `${m01}/${n}.ts`,
       { allowedTags: ['page'], transformFn: snippetToCodeBlock(`html title="${n}.ts"`), outFile: `${outRoot}/hono/${n}_ts.mdx` }
@@ -33,4 +32,13 @@ export function extractHonoSnippets() {
   m01page('m01d04');
   m01page('m01d05');
 
+  // M02:
+  const m02page = n => {
+    const m02 = `${srcRoot}/src/m02jsx`;
+    processSnippet(
+      `${m02}/${n}.tsx`,
+      { allowedTags: ['page'], transformFn: snippetToCodeBlock(`html title="${n}.tsx"`), outFile: `${outRoot}/hono/${n}_tsx.mdx` }
+    );
+  }
+  m02page('m02d01');
 }
