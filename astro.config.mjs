b3c7d99a4edd76819a9305f30edd13baf6b3d32a
Sven Ehrke <sven.ehrke@sven-ehrke.de>
2026-09-12 11:40:33 +0200

    project rename: SSFE -> Hypermedia

--- astro.config.mjs
+++ astro.config.mjs
@@ -19,7 +19,7 @@ export default defineConfig({
   },
 	integrations: [
 		starlight({
-			title: 'Server Side Frontend Patterns',
+			title: 'Hypermedia Patterns',
 			customCss: [
         		'./src/styles/custom.css',
       		],
